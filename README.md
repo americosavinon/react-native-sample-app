921d8a4a714a4f9f22cf605ae8fac9aa1591f78d
Peter Lin <yunfeng.lin@rallyhealth.com>
2019-11-26 14:56:35 -0500

    add jest sample for todo reducer

--- README.md
+++ README.md
@@ -1,29 +1,5 @@
 # react-native-sample-app
+
 Todo List app demo
- - We use this app for demo purpose, simple business logic but touch all the key concepts for jump starter
 
-# How we setup todoist with expo
-
-```
-expo init todoist # (choose managed with blank typescript support)
-yarn add react-native-paper
-yarn add react-native-vector-icons
-react-native link react-native-vector-icons
-```
-
-How to start the project in local mac.
-
-```
-cd todo/ios
-pod install
-
-cd todo
-yarn ios
-```
-
-## react native ant-design part setup notes
-
-```
-yarn add @ant-design/react-native
-react-native link @ant-design/icons-react-native
-```
+- We use this app for demo purpose, simple business logic but touch all the key concepts for jump starter
--- todoist/features/todos/todosSlice.test.ts
+++ todoist/features/todos/todosSlice.test.ts
@@ -1,6 +1,129 @@
-import { addTodo } from './todosSlice';
+import todos, { addTodo, toggleTodo } from './todosSlice';
 
-// adding test example
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+describe('todos reducer', () => {
+  it('should handle initial state', () => {
+    expect(todos(undefined, {})).toEqual([]);
+  });
+
+  it('should handle ADD_TODO', () => {
+    expect(
+      todos([], {
+        type: addTodo.type,
+        payload: {
+          text: 'Run the tests',
+          id: '0',
+        },
+      })
+    ).toEqual([
+      {
+        text: 'Run the tests',
+        completed: false,
+        id: '0',
+      },
+    ]);
+
+    expect(
+      todos(
+        [
+          {
+            text: 'Run the tests',
+            completed: false,
+            id: '0',
+          },
+        ],
+        {
+          type: addTodo.type,
+          payload: {
+            text: 'Use Redux',
+            id: '1',
+          },
+        }
+      )
+    ).toEqual([
+      {
+        text: 'Run the tests',
+        completed: false,
+        id: '0',
+      },
+      {
+        text: 'Use Redux',
+        completed: false,
+        id: '1',
+      },
+    ]);
+
+    expect(
+      todos(
+        [
+          {
+            text: 'Run the tests',
+            completed: false,
+            id: '0',
+          },
+          {
+            text: 'Use Redux',
+            completed: false,
+            id: '1',
+          },
+        ],
+        {
+          type: addTodo.type,
+          payload: {
+            text: 'Fix the tests',
+            id: '2',
+          },
+        }
+      )
+    ).toEqual([
+      {
+        text: 'Run the tests',
+        completed: false,
+        id: '0',
+      },
+      {
+        text: 'Use Redux',
+        completed: false,
+        id: '1',
+      },
+      {
+        text: 'Fix the tests',
+        completed: false,
+        id: '2',
+      },
+    ]);
+  });
+
+  it('should handle TOGGLE_TODO', () => {
+    expect(
+      todos(
+        [
+          {
+            text: 'Run the tests',
+            completed: false,
+            id: '1',
+          },
+          {
+            text: 'Use Redux',
+            completed: false,
+            id: '0',
+          },
+        ],
+        {
+          type: toggleTodo.type,
+          payload: '1',
+        }
+      )
+    ).toEqual([
+      {
+        text: 'Run the tests',
+        completed: true,
+        id: '1',
+      },
+      {
+        text: 'Use Redux',
+        completed: false,
+        id: '0',
+      },
+    ]);
+  });
 });

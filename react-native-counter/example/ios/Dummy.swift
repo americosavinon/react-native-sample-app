18cce6a2e149fdca1ae065d4370a7d6d15e77667
Peter Lin <yunfeng.lin@rallyhealth.com>
2019-12-07 23:18:29 -0500

    removed counter example.

--- react-native-counter/example/ios/Dummy.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Dummy.swift
-//  example
-//
-//  Created by Yunfeng Lin on 12/5/19.
-//  Copyright © 2019 Facebook. All rights reserved.
-//
-
-import Foundation
--- react-native-counter/index.js
+++
@@ -1,5 +0,0 @@
-import { NativeModules } from 'react-native';
-
-const { Counter } = NativeModules;
-
-export default Counter;

5e402076356c0f8bbd958998bc5a8cd3f4c59439
Peter Lin <yunfeng.lin@rallyhealth.com>
2019-12-16 18:44:14 -0500

    finished unit tests for swift class.

--- amwell-react-native-demo/react-native-amwellservice/ios/AmwellserviceTests/AmwellserviceTests.swift
+++ amwell-react-native-demo/react-native-amwellservice/ios/AmwellserviceTests/AmwellserviceTests.swift
@@ -8,19 +8,41 @@
 
 import XCTest
 
+@testable import Amwellservice
+
 class AmwellserviceTests: XCTestCase {
+    
+    var target: Amwellservice!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        target = Amwellservice()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        target = nil
+        super.tearDown()
     }
 
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let exp = expectation(description: "Test setCount Async function!")
+
+        target.setCount(_: 3, resolver: { (value) in
+            XCTAssertTrue(value! as? Int == 3)
+            
+            // Don't forget to fulfill the expectation in the async callback
+            exp.fulfill()
+            
+        }, rejecter: { (error, type, Error) in
+            XCTAssertTrue(error! as? String == "init_fail")
+            exp.fulfill()
+        })
+        
+        waitForExpectations(timeout: 0.3, handler: nil)
     }
 
     func testPerformanceExample() {

57b5718ab63887562f3c5b39dd7a87a5e2148bb6
Peter Lin <yunfeng.lin@rallyhealth.com>
2020-01-28 08:26:28 -0500

    add expo navigation example.

--- amwell-react-native-demo/ignitedemo/app/navigation/root-navigator.ts
+++ amwell-react-native-demo/ignitedemo/app/navigation/root-navigator.ts
@@ -1,7 +1,6 @@
 import { createStackNavigator } from "react-navigation-stack"
 import { PrimaryNavigator } from "./primary-navigator"
-import {
-} from "../screens" // eslint-disable-line @typescript-eslint/no-unused-vars
+import {} from "../screens" // eslint-disable-line @typescript-eslint/no-unused-vars
 
 export const RootNavigator = createStackNavigator(
   {
